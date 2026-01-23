96ddddd63218c5ffa0bca8180b82e1649f4e3014
Paulo André <andrepaulo921@gmail.com>
2026-01-23 09:46:13 -0300

    solved 1037

---
+++ codes/Intervalo.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // 1037
+
+using namespace std;
+
+// [0,25], (25,50], (50,75], (75,100]
+
+int main() {
+    double num;
+
+    cin >> num;
+
+    if (num == 25 ) {
+        cout << "Intervalo [0,25]" << endl;
+    } else if (num > 25 && num <= 50) {
+        cout << "Intervalo (25,50]" << endl;
+    } else if (num > 50 && num <= 75) {
+        cout << "Intervalo (50,75]" << endl;
+    } else if (num > 75 && num <= 100) {
+        cout << "Intervalo (75,100]" << endl;
+    } else {
+        cout << "Fora de intervalo" << endl;
+    }
+
+    return 0;
+}
