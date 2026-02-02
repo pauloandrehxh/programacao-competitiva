cd819955613f231816e9c70f5bc53a233e005397
Paulo André <andrepaulo921@gmail.com>
2026-02-02 11:30:33 -0300

    solved 1067

---
+++ codes/Números_Ímpares.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+
+    cin >> x;
+
+    for (int i = 1; i <= x; i+=2) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
